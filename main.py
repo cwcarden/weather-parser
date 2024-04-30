d0fc11970215a77410b06fc31094a76ef9cc054c
charles <charles@charlescarden.com>
2024-04-29 21:01:39 -0500

    Add is_freezing and is_windy functions

--- main.py
+++ main.py
@@ -63,16 +63,25 @@ finally:
     conn.close()
     #last_rain = (data[0]['lastRain'])
 
-# Returns true if has been 48 hours since last rain
-    def is_48_hours_old():
+    # Returns true if has been 48 hours since last rain
+    def rain_last48():
         last_rain = (data[0]['lastRain'])
         date = parser.parse(last_rain)
         current_date = datetime.now(timezone.utc)
         time_difference = current_date - date
         return (time_difference.total_seconds() >= 48 * 3600) 
 
-
+    # Returns True if is currently raining
     def is_raining():
         if (data[0]['eventrainin']) == 0:
             return False
-        
+
+    # Returns True if below freezing. Set to 33 as precaution
+    def is_freezing():
+        if (data[0]['tempf'] < 33):
+            return True
+
+    # Returns True if too windy 
+    def is_windy():
+        if (data[0]['windspeedmph'] > 15):
+            return True
